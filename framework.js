ec51a2b7f85ef2a393c6ea33c7c5d7ed5fe6dd00
Kai Dorschner <me@krnl.de>
2015-09-16 21:06:26 +0200

    i18n wip

--- framework.js
+++ framework.js
@@ -1,9 +1,32 @@
 /* eslint-env node, es6 */
 "use strict";
-function sayHelloTo(name) {
+const format = require("util").format;
+module.exports.sayHelloTo = function sayHelloTo(name) {
   let ret = ''
-  for (let i = 20; i--;) ret += '<li>Jo <em>' + name + '</em>!</li>'
+  for (let i = 20; i--;) ret += '<li>Hello <em>' + name + '</em>!</li>'
   return ret
 }
 
-module.exports.sayHelloTo = sayHelloTo
+
+const translation = {
+  'sheeps': '%d sheep(s)'
+}
+
+module.exports.t = function t(token, count, ...rest) {
+  const formatStrings = count !== undefined ? [count].concat(rest) : rest;
+  let template = translate(token);
+  if (count !== undefined) template = pluralize(template);
+  return format(translated, formatStrings);
+  return token;
+}
+
+function translate(token) {
+  return translation[token];
+}
+
+function pluralize(text, count) {
+  const parts = text.match(/(.+?)\((.+?)\)/)
+  const plural = parts ? parts[2] : ''
+  const singular = plural.split('|')[0] == plural ? parts[1] : plural.split('|')[0]
+  console.log({parts, plural, singular});
+}
