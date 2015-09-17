2267a579da70822a62ebc6eef85d85bbec6a84f3
Kai Dorschner <me@krnl.de>
2015-09-17 10:14:59 +0200

    removed wip stuff.

--- framework.js
+++ framework.js
@@ -8,25 +8,25 @@ module.exports.sayHelloTo = function sayHelloTo(name) {
 }
 
 
-const translation = {
-  'sheeps': '%d sheep(s)'
-}
+// const translation = {
+//   'sheeps': '%d sheep(s)'
+// }
 
-module.exports.t = function t(token, count, ...rest) {
-  const formatStrings = count !== undefined ? [count].concat(rest) : rest;
-  let template = translate(token);
-  if (count !== undefined) template = pluralize(template);
-  return format(translated, formatStrings);
-  return token;
-}
+// module.exports.t = function t(token, count, ...rest) {
+//   const formatStrings = count !== undefined ? [count].concat(rest) : rest;
+//   let template = translate(token);
+//   if (count !== undefined) template = pluralize(template);
+//   return format(translated, formatStrings);
+//   return token;
+// }
 
-function translate(token) {
-  return translation[token];
-}
+// function translate(token) {
+//   return translation[token];
+// }
 
-function pluralize(text, count) {
-  const parts = text.match(/(.+?)\((.+?)\)/)
-  const plural = parts ? parts[2] : ''
-  const singular = plural.split('|')[0] == plural ? parts[1] : plural.split('|')[0]
-  console.log({parts, plural, singular});
-}
+// function pluralize(text, count) {
+//   const parts = text.match(/(.+?)\((.+?)\)/)
+//   const plural = parts ? parts[2] : ''
+//   const singular = plural.split('|')[0] == plural ? parts[1] : plural.split('|')[0]
+//   console.log({parts, plural, singular});
+// }
